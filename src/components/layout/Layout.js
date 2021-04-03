82b7582958f606bb1aea2c79a61836647d3074ec
Amanjot Singh <amanjotsingh260@gmail.com>
2021-04-03 11:56:44 +0530

    fixed: reference error for localstorage for ssr (#3)
    * added hello world
    
    * added contact us and home page
    
    * removed footer, added about in header and updated Layout css
    
    * fixed: reference error for localstorage for ssr

--- src/components/layout/Layout.js
+++ src/components/layout/Layout.js
@@ -1,14 +1,14 @@
-import React, { useState } from "react"
+import React, { useState, useEffect } from "react"
 import { createMuiTheme, ThemeProvider, makeStyles, createStyles } from '@material-ui/core/styles';
 import CssBaseline from "@material-ui/core/CssBaseline";
 import ThemeContext from './../../components/context';
 
 
 import Header from "../header/Header";
-import Footer from '../footer/Footer';
+// import Footer from '../footer/Footer';
 import './layout.css';
 
-const useStyles = makeStyles((theme) => { 
+const useStyles = makeStyles((/* theme */) => { 
   return createStyles({
     root: {
       height: '90vh',
@@ -22,12 +22,11 @@ const useStyles = makeStyles((theme) => {
 
 
 const Layout = ({ children }) => {
-  const lsTheme = localStorage.getItem('themeType')  || 'default';
-  const [themeType, setThemeType] = useState(lsTheme);
-  const isDarkState = lsTheme === "dark";
+  const [themeType, setThemeType] = useState('default');
+  const isDarkState = themeType === "dark";
   const  classes = useStyles();
   const palletType = isDarkState ? "dark" : "light";
-// #16a085, #27ae60, #2980b9, #8e44ad, #8e44ad, #2c3e50, https://flatuicolors.com/palette/defo (2nd row)
+  // #16a085, #27ae60, #2980b9, #8e44ad, #8e44ad, #2c3e50, https://flatuicolors.com/palette/defo (2nd row)
   const theme = createMuiTheme({
     palette: {
       type: palletType,
@@ -42,9 +41,14 @@ const Layout = ({ children }) => {
       },
     }
   });
-
-
-const contextValues = { themeType, setThemeType, isDarkState }
+  
+  
+  const contextValues = { themeType, setThemeType, isDarkState }
+  
+  useEffect(() =>{
+    const lsTheme = localStorage.getItem('themeType')  || 'default';
+    setThemeType(lsTheme);
+  }, [])
 
   return (
     <ThemeContext.Provider value={contextValues} >
